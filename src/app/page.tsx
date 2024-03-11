a9a47dd60dbe216b721f297d07043112a0b51932
bhuwan-darai <dr.darai29@gmail.com>
2024-03-11 10:31:12 +0545

    adding monaco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-import Image from "next/image";
+import Link from "next/link";
+import { db } from "@/db";
 
-export default function Home() {
-  return <div>Hello</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <Link
+        key={snippet.id}
+        className="flex justify-between items-center p-2 rounded border"
+        href={`/snippet/${snippet.id}`}
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex m-2 justify-between">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippet/new" className="border p-2  rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex gap-2 flex-col">{renderedSnippets}</div>
+    </div>
+  );
 }
